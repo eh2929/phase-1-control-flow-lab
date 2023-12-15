a82a834e589a9c262d00e2a214795a32c824ef61
Ethan <ethanhough29@gmail.com>
2023-12-15 11:55:03 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let greeting 
+  if (feet < 400) {
+    greeting = "This one is on me!"
+  }
+  else if (feet < 2000) {
+    greeting = "That will be twenty bucks."
+  } 
+  else if (feet < 2500) {
+    greeting = "I will gladly take your thirty bucks."
+  }
+  else {
+    greeting = "No can do."
+  }
+return greeting
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let answer = (city == "NYC") ? 'Ok, sounds good.' : 'No go.'
+  return answer
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(text){
+    switch (text) {
+    case "generous" :
+      text = "Thank you so much.";
+      break;
+    case "not as generous" :
+      text = "Thank you.";
+      break;
+    default:
+      text = "Bye."
+    
+  }
+  return text
 }
